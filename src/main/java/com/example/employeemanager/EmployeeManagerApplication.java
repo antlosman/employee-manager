441200561c5dad84dcb2c441042e334f1dd9c683
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-10-04 10:48:51 +0300

    [add] - added properties to Employee entity and connection to database

--- src/main/java/com/example/employeemanager/EmployeeManagerApplication.java
+++ src/main/java/com/example/employeemanager/EmployeeManagerApplication.java
@@ -2,9 +2,8 @@ package com.example.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class EmployeeManagerApplication {
 
 	public static void main(String[] args) {
